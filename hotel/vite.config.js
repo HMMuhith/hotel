c85dec00c461ef0b55d1a3a3a589361c6b8fb20b
Muhith <muhithkhan8@gmail.com>
2025-03-17 01:33:51 +0600

    added

--- hotel/vite.config.js
+++ hotel/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    svgr({
+      
+      include:'**/*.svg',
+    })
   ],
   resolve:{
     extensions:['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json']
