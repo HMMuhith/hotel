a7a2c52211746a0c6d27aaca86646cd3484a05cc
HM Muhith <45318851+HMMuhith@users.noreply.github.com>
2025-06-22 15:30:55 +0600

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use(express.static(path.join(__dirname,'public')))
 if(process.env.NODE_ENV==='production'){
   app.use(express.static(path.join(__dirname,'hotel','dist')))
 
-  app.get('*',(req,res)=>{
+  app.get('/{*splat}',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'hotel','dist','index.html'))
    
   })
